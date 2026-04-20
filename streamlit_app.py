3d1efa090c7e2f51efc23dde8e29a3600517179c
quadrikel <e.kozlikin@gmail.com>
2026-04-20 16:32:19 +0200

    Update API URL for Smoothiefroot service

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,5 +38,5 @@ if ingredients_list:
 
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("[http://my.smoothiefroot.com/api/fruit/watermelon](http://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
