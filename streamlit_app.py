a6c058e0151cb51fd7f526fa3eaae077f85e22d1
quadrikel <e.kozlikin@gmail.com>
2026-04-20 16:44:28 +0200

    Update API endpoint for fruit data retrieval

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,5 +38,5 @@ if ingredients_list:
 
 
 import requests  
-smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/orange")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/orange")  
 st.text(smoothiefroot_response)
